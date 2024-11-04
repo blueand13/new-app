024f64e454c3f2b8899ec9cebe5907505aac9b98
Ahmet Şen <ahmetapor22@hotmail.com>
2024-11-04 12:34:37 +0000

    succes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          wwwwwwwwwwwwwww
         </a>
       </header>
     </div>
